c5461b83ba86fafa70a824ddee2f4c0785fffd75
m.beliakouski <mikhail.beliakouski@gmail.com>
2023-12-15 16:15:17 +0300

    add LoginPage and transitions between pages

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    REGISER_FORM = (By.CSS_SELECTOR,"form#register_form.well")
+    LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form.well")
---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        assert "https://selenium1py.pythonanywhere.com/en-gb/" != current_url, "It's wrong link" 
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form not provided"
+        
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISER_FORM), "Registration form not provided"
+        
