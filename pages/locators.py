f5526eeca69d4d4c8ef292df62cf2e150ade7fa2
m.beliakouski <mikhail.beliakouski@gmail.com>
2023-12-19 17:05:02 +0300

    add data independent processing

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators():
 class LoginPageLocators():
     REGISER_FORM = (By.CSS_SELECTOR,"form#register_form.well")
     LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form.well")
+
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOK_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    NAME_BOOK_ADDET_TO_BASKET = (By.CSS_SELECTOR,".alert-safe:nth-of-type(1) .alertinner strong")
+    BASKET_TOTAL = (By.CSS_SELECTOR, '.alert-info .alertinner strong')
+    COST_BOOK = (By.CSS_SELECTOR, 'p.price_color')
+    
